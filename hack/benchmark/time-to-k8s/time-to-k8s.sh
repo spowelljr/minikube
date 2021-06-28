bb958a93cbacf02191af87b4c815633ef52a057d
Steven Powell <spowellgrr@gmail.com>
2021-06-28 14:05:24 -0700

    123

--- hack/benchmark/time-to-k8s/time-to-k8s.sh
+++ hack/benchmark/time-to-k8s/time-to-k8s.sh
@@ -19,7 +19,7 @@ set -e
 install_kind() {
 	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
 	chmod +x ./kind
-	sudo mv ./kind /usr/local
+	sudo mv ./kind /usr/local/bin/kind
 }
 
 install_k3d() {
